d6003fd2658e4a689729a47aa11a26ff013c8ef7
ivettemalagon <71296531+ivettemalagon@users.noreply.github.com>
2020-12-16 05:49:46 -0500

    Update

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ api = FastAPI()     #Crea la aplicación
 from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-    "http://localhost", "http://localhost:8080", "https://cajero-app-ivette.herokuapp.com/",
+    "http://localhost", "http://localhost:8080", "https://cajero-app-ivette.herokuapp.com",
 ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
